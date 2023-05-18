998bfa5b6638d06f6e5aaea489b82bfd1680669f
Hakim Ben Hadj  Brahim <bhbhakim123@gmail.com>
2023-05-18 02:09:10 +0100

    first commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:simple_modern/main.dart';
+import 'package:flutter_log_signup_home_project/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
